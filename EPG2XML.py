180d82bbbc4127ee8e76221dd6525b37fbfc63ac
deathbybandaid <github@deathbybandaid.net>
2020-09-22 19:51:38 -0400

    test

--- EPG2XML.py
+++ EPG2XML.py
@@ -8,13 +8,18 @@ import urllib.parse
 import urllib.request
 import xml.etree.ElementTree as ET
 
+from L2PTools import clean_exit
+
 
 def epgServerProcess(script_dir, config, location):
     dummyxml(script_dir, config, location)
-    while True:
-        print("Fetching EPG for DMA " + str(location["DMA"]) + ".")
-        mainepg(script_dir, config, location)
-        time.sleep(config.config["dev"]["epg_delay"])
+    try:
+        while True:
+            print("Fetching EPG for DMA " + str(location["DMA"]) + ".")
+            mainepg(script_dir, config, location)
+            time.sleep(config.config["dev"]["epg_delay"])
+    except KeyboardInterrupt:
+        clean_exit()
 
 
 def dummyxml(script_dir, config, location):
